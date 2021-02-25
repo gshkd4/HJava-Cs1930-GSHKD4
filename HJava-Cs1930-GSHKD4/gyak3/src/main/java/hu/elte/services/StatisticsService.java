3b76eafa61302d3412c852b963dd737aa0bff57f
gshkd4 <gshkd4@inf.elte.hu>
2021-02-25 21:11:25 +0100

    Modify StatisticsService

--- HJava-Cs1930-GSHKD4/gyak3/src/main/java/hu/elte/services/StatisticsService.java
+++ HJava-Cs1930-GSHKD4/gyak3/src/main/java/hu/elte/services/StatisticsService.java
@@ -4,13 +4,21 @@ import java.util.List;
 
 public class StatisticsService {
 
+    Database database;
+
+    public StatisticsService(Database database) {
+        this.database = database;
+    }
+
     // test it!
     public int countPersonsWithName(String name) {
-        Database database = new DatabaseImpl();
         database.connect();
+
         List<Person> persons = database.findPersonsWithName(name);
         int count = persons.size();
+
         database.disconnect();
+
         return count;
     }
 }
