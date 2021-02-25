b28860bfc6e42e6cf7eaf56f822c32f0668f8999
gshkd4 <gshkd4@inf.elte.hu>
2021-02-25 21:12:01 +0100

    Update StatisticsServiceTest With Mock Database

--- HJava-Cs1930-GSHKD4/gyak3/src/test/java/hu/elte/services/StatisticsServiceTest.java
+++ HJava-Cs1930-GSHKD4/gyak3/src/test/java/hu/elte/services/StatisticsServiceTest.java
@@ -1,17 +1,37 @@
 package hu.elte.services;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
 
 public class StatisticsServiceTest {
+
+    @Mock
+    private Database database;
 
     private StatisticsService statisticsService;
 
     @BeforeEach
     public void setUp() {
-        statisticsService = new StatisticsService();
+        database = mock(Database.class);
+        statisticsService = new StatisticsService(database);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        verifyNoMoreInteractions(database);
     }
 
     @ParameterizedTest(name = "{0} = name, {1} = expectedCount")
@@ -21,8 +41,25 @@ public class StatisticsServiceTest {
             "TEST_NAME_1, 2"
     })
     public void testCountPersonsWithName(String name, int expectedCount) {
-        int actualCount = statisticsService.countPersonsWithName(name);
+        doNothing().when(database).connect();
+        when(database.findPersonsWithName(name)).thenReturn(createPersonList(name, expectedCount));
+        doNothing().when(database).disconnect();
 
+        int actualCount = statisticsService.countPersonsWithName(name);
         Assertions.assertEquals(expectedCount, actualCount);
+
+        verify(database).connect();
+        verify(database).findPersonsWithName(name);
+        verify(database).disconnect();
+    }
+
+    private List<Person> createPersonList(String name, int count) {
+        List<Person> personList = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            personList.add(new Person(name));
+        }
+
+        return personList;
     }
 }
